6b0b4a3b1d6bf61bc7dffa67607fbcc48f24d7a1
Rudolf Bauer <github@rudi-bauer.com>
2019-11-18 02:24:04 +0100

    refactor: add missing dependencies.

--- setup.py
+++ setup.py
@@ -3,19 +3,12 @@
 from setuptools import setup
 
 setup(name='genesisclient',
-      version='0.0.7',
+      version='0.0.8',
       description='Genesis (DeStatis et. al.) client for Python',
       author='Marian Steinbach',
       author_email='marian@sendung.de',
-      url='https://github.com/marians/genesisclient',
+      url='https://github.com/rudolf-bauer/genesisclient.git',
       license="MIT",
       packages=['genesisclient'],
-      install_requires=[
-        'lxml',
-        'suds'],
-      entry_points={
-        'console_scripts': [
-            'genesiscl = genesisclient:main'
-        ]
-      }
+      install_requires=['lxml', 'zeep', 'requests', 'tempfile']
 )
