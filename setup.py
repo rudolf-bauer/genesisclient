5a44dbfd67f768972c9bf08632f37f0ececbc6b6
Rudolf Bauer <github@rudi-bauer.com>
2019-11-20 00:13:03 +0100

    docs: add long description for pypi

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ from setuptools import setup
 setup(name='pygenesis',
       version='0.0.1',
       description='PyGenesis - get access to german statistical offices using the genesis API',
+      long_description='The aim of the project is to provide simplified data access to German statistical offices in '
+                       'Python using the so-called '
+                       '[Genesis API](https://www.destatis.de/EN/Service/OpenData/_node.html).',
+      long_description_content_type="text/markdown",
       author='Rudolf Bauer',
       author_email='github@rudi-bauer.com',
       url='https://github.com/rudolf-bauer/genesisclient.git',
